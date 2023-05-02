f88216070b61b0f748fbd8ba8cb2b5a059a8a765
Ian <istewart17@my.bcit.ca>
2023-05-02 14:37:37 -0700

    adds database workouts to workout page dynamically

--- controller/gym_controller.js
+++ controller/gym_controller.js
@@ -2,13 +2,15 @@ let database = require("../equipment");
 
 let gymController = {
     index: (req, res) => {
-        res.render("gym/index", {database})
+        let equipment = database["equipment"]
+        res.render("gym/index", {equipment})
     },
     home: (req, res) => {
         res.render("gym/homepage")
     },
     workout: (req, res) => {
-        res.render("gym/workoutpage")
+        let workouts = database["workouts"]
+        res.render("gym/workoutpage", {workouts})
     },
     progress: (req, res) => {
         res.render("gym/progress")
