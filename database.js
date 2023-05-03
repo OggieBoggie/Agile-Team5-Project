ced6d25816de92f03c0218090e753196423c4117
Victor Tran <victortran1449@gmail.com>
2023-05-02 23:54:54 -0700

    adds workout info for each workout and some interface changes

--- database.js
+++ database.js
@@ -1,23 +1,26 @@
 let Database = {
-    'equipment': ["dumbbell", "swiss ball"],
-     'workouts': [
-             {
-                 'workout': 'Overhead Presses',
-                 'muscleGroups' : ["shoulder", 'back', 'core'],
-                 'equipment' : 'dumbbells',
-                 'description' : 'standing straight, lift dumbbells above head'
+    equipment: ["dumbbell", "swiss ball"],
+     workouts: [
+             {      
+                id : 1,
+                 workout: 'Overhead Presses',
+                 muscleGroups : ["shoulder", 'back', 'core'],
+                 equipment : 'dumbbells',
+                 description : 'standing straight, lift dumbbells above head'
              },
              {
-                 'workout': 'deadlifts',
-                 'muscleGroups' : ["legs", 'back'],
-                 'equipment' : 'barbell',
-                 'description' : 'pick up barbell'
+                id : 2,
+                 workout: 'deadlifts',
+                 muscleGroups : ["legs", 'back'],
+                 equipment : 'barbell',
+                 description : 'pick up barbell'
              },
              {
-                 'workout': 'plank',
-                 'muscleGroups' : ['core'],
-                 'equipment' : 'none',
-                 'description' : 'rest on your toes and elbows with a straight back for 1 minute'
+                id : 3,
+                 workout: 'plank',
+                 muscleGroups : ['core'],
+                 equipment : 'none',
+                 description : 'rest on your toes and elbows with a straight back for 1 minute'
              }
          ]
      }  
