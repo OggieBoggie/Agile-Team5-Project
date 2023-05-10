96b530129cd37dd745e7f0d0a98b068410bdad38
Victor Tran <victortran1449@gmail.com>
2023-05-10 15:33:41 -0700

    adds manage equipment endpoint, add equipment endpoint, and edit equipment endpoint

--- controller/gym_controller.js
+++ controller/gym_controller.js
@@ -4,120 +4,137 @@ let database = require("../database");
 let fs = require("fs");
 let userDatabase = undefined;
 fs.readFile("../userDatabase.json", (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        userDatabase = JSON.parse(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    userDatabase = JSON.parse(data);
+  }
 });
 
 let gymController = {
-    index: (req, res) => {
-        let equipment = database["equipment"];
-        res.render("gym/index", { equipment });
-    },
-    home: (req, res) => {
-        request = req;
-        console.log(req.user)
-        gym = req.user
-        if (req.user.gymAccount) {
-            res.render("gym/gymhomepage"), { gym }
-        } else {
-            res.render("gym/homepage"), { request };
-        }
-    },
-    randomWorkout: (req, res) => {
+  index: (req, res) => {
+    let equipment = database["equipment"];
+    res.render("gym/index", { equipment });
+  },
+  home: (req, res) => {
+    request = req;
+    console.log(req.user);
+    gym = req.user;
+    if (req.user.gymAccount) {
+      res.render("manager/gymhomepage"), { gym };
+    } else {
+      res.render("gym/homepage"), { request };
+    }
+  },
+  randomWorkout: (req, res) => {
+    let randomWorkoutID = Math.round(
+      Math.random() * (database.workouts.length - 1) + 1
+    );
+    randomWorkout = database.workouts.find((workout) => {
+      return workout.id === randomWorkoutID;
+    });
+    let randomWorkoutList = [];
+    for (let e of database.exercises) {
+      if (randomWorkout.exercise_list.includes(e.exercise)) {
+        randomWorkoutList.push(e);
+      }
+    }
+    console.log(randomWorkoutList);
+    res.render("gym/workoutpage", { randomWorkoutList });
+  },
+  progress: (req, res) => {
+    if (!req.user.progress) {
+      req.user.progress = [];
+    }
+    const progressReports = req.user.progress;
+    res.render("gym/progress", { progressReports });
+  },
+  new: (req, res) => {
+    if (!req.user) {
+      res.render("login");
+    } else {
+      const exercises = database["exercises"];
+      res.render("gym/create", { exercises });
+    }
+  },
 
-        let randomWorkoutID = Math.round(
-            Math.random() * (database.workouts.length - 1) + 1
-        );
-        randomWorkout = database.workouts.find((workout) => {
-            return workout.id === randomWorkoutID;
-        });
-        let randomWorkoutList = [];
-        for (let e of database.exercises) {
-            if (randomWorkout.exercise_list.includes(e.exercise)) {
-                randomWorkoutList.push(e);
-            }
-        }
-        console.log(randomWorkoutList);
-        res.render("gym/workoutpage", { randomWorkoutList });
-    },
-    progress: (req, res) => {
-        if (!req.user.progress) {
-            req.user.progress = [];
-        }
-        const progressReports = req.user.progress
-        res.render("gym/progress", { progressReports });
-    },
-    new: (req, res) => {
-        if (!req.user) {
-          res.render("login");
-        } else {
-          const exercises = database["exercises"]
-          res.render("gym/create", { exercises });
-        }
-      },
+  create: (req, res) => {
+    if (!req.user) {
+      return res.render("../views/login");
+    }
+    user = req.user;
+    userDatabase = req.database;
 
-    create: (req, res) => {
-        if (!req.user) {
-            return res.render("../views/login");
-        }
-        user = req.user
-        userDatabase = req.database
+    userdb = userDatabase.users.find((user) => {
+      return user.id === req.user.id;
+    });
 
-        userdb = userDatabase.users.find(user => {return user.id === req.user.id})
+    let progressReport = {
+      date: req.body.date,
+      type: req.body.type,
+      details: req.body.details,
+    };
 
-        let progressReport = {
-            date: req.body.date,
-            type: req.body.type,
-            details: req.body.details,
-        };
+    req.user.progress.push(progressReport);
+    userdb.progress = req.user.progress;
 
-        req.user.progress.push(progressReport);
-        userdb.progress = req.user.progress
-        
+    userDataString = JSON.stringify(userDatabase);
 
-        userDataString = JSON.stringify(userDatabase)
+    fs.writeFile("../userDatabase.json", userDataString, (err) => {
+      if (err) {
+      } else {
+        res.render("gym/progress", { progressReports: req.user.progress });
+      }
+    });
+  },
 
- 
-        fs.writeFile('../userDatabase.json', userDataString, (err) => {
-            if (err) {
-
-            } else {
-
-                res.render("gym/progress", { progressReports: req.user.progress });
-            }
-        })
-    },
-
-
-    calendar: (req, res) => {
-        res.render("gym/calendar")
-    },
-    selectWorkout: (req, res) => {
-        let selectedWorkout = undefined;
-        let workouts = database.workouts;
-        res.render("gym/selectworkout", { workouts, selectedWorkout });
-    },
-    displayWorkout: (req, res) => {
-        let exercises = database["exercises"];
-        let workouts = database["workouts"];
-        let selectedWorkout = database.workouts.find((workout) => {
-            return workout.id == req.params.id;
-        });
-        let exercise_objects = [];
-        for (let e of exercises) {
-            if (selectedWorkout.exercise_list.includes(e.exercise)) {
-                exercise_objects.push(e);
-            }
-        }
-        res.render("gym/selectworkout", {
-            workouts,
-            exercise_objects,
-            selectedWorkout,
-        });
-    },
+  calendar: (req, res) => {
+    res.render("gym/calendar");
+  },
+  selectWorkout: (req, res) => {
+    let selectedWorkout = undefined;
+    let workouts = database.workouts;
+    res.render("gym/selectworkout", { workouts, selectedWorkout });
+  },
+  displayWorkout: (req, res) => {
+    let exercises = database["exercises"];
+    let workouts = database["workouts"];
+    let selectedWorkout = database.workouts.find((workout) => {
+      return workout.id == req.params.id;
+    });
+    let exercise_objects = [];
+    for (let e of exercises) {
+      if (selectedWorkout.exercise_list.includes(e.exercise)) {
+        exercise_objects.push(e);
+      }
+    }
+    res.render("gym/selectworkout", {
+      workouts,
+      exercise_objects,
+      selectedWorkout,
+    });
+  },
+  equipment: (req, res) => {
+    equipment = req.user.equipment;
+    console.log(req.user.equipment);
+    res.render("manager/equipment"), { equipment };
+  },
+  viewEditEquipment: (req, res) => {
+    let equipmentID = req.params.id;
+    let equipment = req.user.equipment.find(function (equipment) {
+      return equipment.id == equipmentID;
+    });
+    res.render("manager/edit", { equipment });
+  },
+  editEquipment: (req, res) => {
+    // implement
+  },
+  viewAddEquipment: (req, res) => {
+    res.render("manager/add");
+  },
+  addEquipment: (req, res) => {
+    // implement
+  },
 };
 
 module.exports = gymController;
